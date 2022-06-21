414632755affd973969642f47de5bfc905710567
Nikita <baynik01@gmail.com>
2022-06-21 10:29:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Main {
+  public static void main(String[] args) {
+    int tab[] = {-3,0,8,9,5,-2,4};
+    Arrays.sort(tab); 
+    System.out.printf("Sorted Array : %s", Arrays.toString(tab));
+  }
+}
